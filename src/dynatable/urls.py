ecebd310d5f2aff65b6a49e7c78c577d9f8ccf3a
blooser <blooser@protonmail.com>
2024-04-01 16:45:20 +0200

    Use swagger for API description
    Signed-off-by: blooser <blooser@protonmail.com>

--- src/dynatable/urls.py
+++ src/dynatable/urls.py
@@ -15,13 +15,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
 
 from dynatable import views
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DynaTable",
+        default_version="0.0.1",
+        description="Dynamic model",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("status/", views.project_status),
     path("api/", include("dynatablebackend.urls")),
 ]
