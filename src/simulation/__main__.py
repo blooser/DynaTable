447955912f34731cd51f9e074638f8d4d9b29001
blooser <blooser@protonmail.com>
2024-04-02 00:19:12 +0200

    Update docstring
    Signed-off-by: blooser <blooser@protonmail.com>

--- src/simulation/__main__.py
+++ src/simulation/__main__.py
@@ -7,13 +7,27 @@ from tests.generator import generator
 logger = get_logger(__name__)
 
 
-def _show(data, host: str):
+def _show(data: dict, host: str):
     """
     Displays the table ID and its rows in a formatted manner.
 
     Args:
         data (dict): A dictionary containing 'table_id' and 'rows' of a table.
-        host (str): A host URI
+        host (str): The URI of the host where the table is located.
+
+    Example:
+        A sample output might look like this:
+
+            =========================
+            Table: '2PCYdAwfB3iPvchWbj5DmY'
+            Rows:
+              [1, 'Julia Kowalczyk', 71.0, 32.0, 39.0, 'Adrian Lewandowski', 'Kasia Szymański', 'Szymon Nowak']
+              [2, 'Szymon Nowak', 41.0, 82.0, 58.0, 'Marcin Lewandowski', 'Piotr Wójcik', 'Szymon Nowak']
+              [3, 'Piotr Wójcik', 33.0, 49.0, 19.0, 'Tomasz Kowalski', 'Piotr Nowak', 'Anna Nowak']
+              [4, 'Piotr Wójcik', 77.0, 70.0, 81.0, 'Kasia Szymański', 'Julia Kowalczyk', 'Piotr Wójcik']
+              [5, 'Julia Kowalczyk', 94.0, 38.0, 37.0, 'Szymon Wiśniewski', 'Kasia Wiśniewski', 'Ola Wójcik']
+
+            Check API: http://localhost:8000/api/table/2PCYdAwfB3iPvchWbj5DmY/rows
     """
     table_id = data["table_id"]
     rows = data["rows"]
